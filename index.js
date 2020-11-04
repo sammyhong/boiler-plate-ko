33c25d1baa664c6d5de219c89089cf06641ed0c2
sammyhong <toadl21@naver.com>
2020-11-04 15:39:50 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+const config = require('./config/key')
+
+// application/x-www-form-urlended
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sameehong:sameehong@boilerplate.3tsfz.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요? 반갑습니다. 냠'))
+
+app.post('/register', (req, res) => {
+
+// 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+// 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
